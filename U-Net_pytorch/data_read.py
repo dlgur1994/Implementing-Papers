76524a6dd177f8c11ebb13ed59667f5f5c7f7ef4
dlgur1994 <21400611@handong.edu>
2021-11-05 21:59:09 +0900

    UNet ing  ng ing 21/11/05

---
+++ U-Net_pytorch/data_read.py
@@ -0,0 +1,41 @@
+## importing library
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+## loading data
+dir_data = './datasets'
+
+name_label = 'train-labels.tif'
+name_input = 'train-volume.tif'
+
+img_label = Image.open(os.path.join(dir_data, name_label))
+img_input = Image.open(os.path.join(dir_data, name_input))
+
+ny, nx = img_label.size
+nframe = img_label.n_frames
+
+## spliting dataset
+nframe_train = 24
+nframe_val = 3
+nframe_test = 3
+
+dir_save_train = os.path.join(dir_data, 'train')
+dir_save_val = os.path.join(dir_data, 'val')
+dir_save_test = os.path.join(dir_data, 'test')
+
+if not os.path.exists(dir_save_train):
+    os.makedirs(dir_save_train)
+
+if not os.path.exists(dir_save_val):
+    os.makedirs(dir_save_val)
+
+if not os.path.exists(dir_save_test):
+    os.makedirs(dir_save_test)
+
+## making random frames
+id_frame = np.arange(nframe)
+np.random.shuffle(id_frame)
+
+##
