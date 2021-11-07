76953b796129cd903890c5d1675420840c9ea366
dlgur1994 <21400611@handong.edu>
2021-11-07 22:52:49 +0900

    UNet ing 21/11/07

--- U-Net_pytorch/train.py
+++ U-Net_pytorch/train.py
@@ -1,5 +1,7 @@
 ## importing libraries
 import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 import torch
@@ -87,7 +89,7 @@ class UNet(nn.Module):
         self.dec1_2 = CBR2d(in_channels=2*64, out_channels=64)
         self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
 
-        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
 
     def forward(self, x):
         enc1_1 = self.enc1_1(x)
@@ -133,3 +135,239 @@ class UNet(nn.Module):
         x =self.fc(dec1_1)
 
         return x
+
+
+## data loader
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+
+    def __getitem__(self, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+
+        label = label/255.0
+        input = input/255.0
+
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+
+        data = {'input':input, 'label':label}
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data
+
+# ## testing Data loader
+# dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'))
+#
+# ## taking a variable
+# data = dataset_train.__getitem__(0)
+#
+# input = data['input']
+# label = data['label']
+#
+# ## show an example
+# plt.subplot(121)
+# plt.imshow(input.squeeze())
+#
+# plt.subplot(122)
+# plt.imshow(label.squeeze())
+#
+# plt.show()
+
+# transform function
+class ToTensor(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        label = label.transpose((2, 0, 1)).astype(np.float32)
+        input = input.transpose((2, 0, 1)).astype(np.float32)
+
+        data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
+
+        return data
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean =mean
+        self.std = std
+
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        input = (input - self.mean) / self.std
+
+        data = {'label': label, 'input': input}
+
+        return data
+
+class RandomFlip(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        if np.random.rand() > 0.5:
+            label = np.fliplr(label)
+            input = np.fliplr(input)
+
+        if np.random.rand() > 0.5:
+            label = np.flipud(label)
+            input = np.flipud(input)
+
+        data = {'label': label, 'input': input}
+
+        return data
+
+
+## training network
+transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
+
+dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=8)
+
+dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
+loader_val = DataLoader(dataset_train, batch_size=batch_size, shuffle=False, num_workers=8)
+
+## creating network
+net = UNet().to(device)
+
+## defining loss function
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+
+## defining optimizer
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+## defining variables
+num_data_train = len(dataset_train)
+num_data_val = len(dataset_val)
+
+num_batch_train = np.ceil(num_data_train / batch_size)
+num_batch_val = np.ceil(num_data_val / batch_size)
+
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0,2,3,1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * (x > 0.5)
+
+writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+## storing network
+def save(ckpt_dir, net, optim, epoch):
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()},
+               "./%s/model_epoch%d.pth" % (ckpt_dir, epoch))
+
+
+## loading network
+def load(ckpt_dir, net, optim):
+    if not os.path.exists(ckpt_dir):
+        epoch = 0
+        return net, optim, epoch
+
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+    dict_model = torch.load('./%s/%s' % (ckpt_dir, ckpt_lst[-1]))
+
+    net.load_state_dict(dict_model['net'])
+    optim.load_state_dict(dict_model['optim'])
+    epoch = int(ckpt_lst[-1].split('epoch')[1].split('pth')[0])
+
+    return net, optim, epoch
+
+
+## training
+st_epoch = 0
+net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+for epoch in range(st_epoch + 1, num_epoch + 1):
+    net.train()
+    loss_arr = []
+
+    for batch, data in enumerate(loader_train, 1):
+        # forward pass
+        label = data['label'].to(device)
+        input = data['input'].to(device)
+
+        output = net(input)
+
+        # backward pass
+        optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        loss.backward()
+
+        optim.step()
+
+        # calculate loss function
+        loss_arr += [loss.item()]
+
+        print("Train: EPOCH %04d / %04d | BATCH %04d \ %04d | LOSS %.4f"
+              % (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+        # saving Tensorboard
+        label = fn_tonumpy(label)
+        input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+        output = fn_tonumpy(fn_class(output))
+
+        writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+
+    writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    with torch.no_grad():
+        net.eval()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_val, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = net(input)
+
+            # calculate loss function
+            loss = fn_loss(output, label)
+
+            loss_arr += [loss.item()]
+
+            print("Validation: EPOCH %04d / %04d | BATCH %04d \ %04d | LOSS %.4f"
+                  % (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
+
+            # saving Tensorboard
+            label = fn_tonumpy(label)
+            input = fn_tonumpy(fn_denorm(input))
+            output = fn_tonumpy(fn_class(output))
+
+            writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+
+    writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    if epoch % 5 == 0:
+        save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+writer_train.close()
+writer_val.close()
